086d24cfecd645ebbb2774eaa4eda47b3653ba82
rebosante <jay@rebosante.com>
2023-03-08 23:00:39 +0100

    update translations

--- pages/massage.vue
+++ pages/massage.vue
@@ -7,31 +7,13 @@
                     <img class="tm-logo-brand" src="~/assets/images/logo.svg">
                 </div>
             </header>
-            <p class="tm-hero-text">
-                El arte del bienestar físico y mental
-                <br/>
-                <strong>Tratamientos personalizados</strong>
-            </p>
+            <p class="tm-hero-text" v-html="$t('massages.intro')" />
         </div>
         <div class="item" data-desktop-seq-no="2" data-mobile-seq-no="4">
             <img src="~/assets/images/image-01.jpg" alt="Image" class="tm-img-left">
         </div>
         <div class="item tm-bg-secondary tm-text-white tm-block tm-block-wider tm-block-pad tm-block-left-2"
-            data-desktop-seq-no="3" data-mobile-seq-no="5">
-            <h2>Masaje Deportivo</h2>
-            <p>Sus objetivos consisten en la prevenci&oacute;n y tratamiento de lesiones deportivas:</p>
-            <ul>
-            <li>Prepara el tejido muscular para el esfuerzo deportivo, manteni&eacute;ndolo en un estado &oacute;ptimo.</li>
-            <li>Previene las lesiones tanto en el entrenamiento como en la competici&oacute;n.</li>
-            <li>Favorece la recuperaci&oacute;n de las lesiones que se presenten.</li>
-            </ul>
-            <br />
-            <h2>masaje energetico</h2>
-            <p>Tratamiento reequilibrante con aceite esencial de menta. Es un masaje de cuerpo completo, con presi&oacute;n en los puntos energ&eacute;ticos de los pies y los o&iacute;dos. Da una sensaci&oacute;n de energ&iacute;a renovadora.</p>
-            <br />
-            <h2>tratamiento facial</h2>
-            <p>Ligero masaje muy relajante de los puntos linf&aacute;ticos de la cara, liberaci&oacute;n de los musculos y estiramiento de los mismos. Descaptaciones de las articulaciones faciales y de la cabeza. Masaje final descongestionante de ojeras.</p>
-        </div>
+            data-desktop-seq-no="3" data-mobile-seq-no="5" v-html="$t('massages.sport')" />
         <div class="item" data-desktop-seq-no="4" data-mobile-seq-no="8">
             <img src="~/assets/images/image-17.jpg" alt="Image" class="tm-img-left">
         </div>
@@ -40,16 +22,8 @@
             <img src="~/assets/images/image-11.jpg" alt="Image">
         </div>
         <div class="item tm-block-right" data-desktop-seq-no="7" data-mobile-seq-no="3">
-            <div class="tm-block-right-inner tm-bg-primary-light tm-text-white tm-block tm-block-wider tm-block-pad">
-                <h2>Masaje Descontracturante / Terap&eacute;utico</h2>
-                <p>Incre&iacute;ble tratamiento para aliviar el estr&eacute;s. un masaje de cuerpo completo
-                    profundamente relajante con movimientos suaves y profundos. Al principio sin aceite usando una
-                    t&eacute;cnica de fascioterapia para estirar el m&uacute;sculo y luego con aceite para inducir un
-                    estado de calma total. Se puede intercalar con t&eacute;cnicas de reflexolog&iacute;a craneo sacral y podal.</p>
-                <br />
-                <p>An incredible stress relief treatment. A deeply relaxing full body massage with soft and deep movements. It starts off without oil, using a fasciotherapy technique to stretch muscles, and then moves on to use oil to induce a complete state of calm. We can mix with craneo, sacral and podal reflexology technique.</p>
+            <div class="tm-block-right-inner tm-bg-primary-light tm-text-white tm-block tm-block-wider tm-block-pad" v-html="$t('massages.therapeutic')" />
                 <!-- -->
-            </div>
         </div>
 
         <div class="item" data-desktop-seq-no="8" data-mobile-seq-no="6">
