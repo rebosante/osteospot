26676b841fb519547c14afe5836827635a2f556d
rebosante <jay@rebosante.com>
2023-03-16 00:29:05 +0100

    added contact form with nodemailer

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,129 +1,150 @@
 <template>
     <div id="contact" class="item tm-bg-white tm-block tm-form-section" data-desktop-seq-no="9" data-mobile-seq-no="7">
-            <div class="tm-form-container tm-block-pad tm-pb-0">
-                <header>
-                    <h2 class="tm-text-uppercase tm-text-gray-light tm-mb">
-                        {{ $t('contact') }}
-                    </h2>
-                </header>
-                <form @submit.prevent="submitForm" class="tm-contact-form">
-                    <div class="tm-form-group">
-                    <input v-model.trim="name" type="text" id="contact_name" name="contact_name" class="form-control" placeholder="Name">
+        <div class="tm-form-container tm-block-pad tm-pb-0">
+            <header>
+                <h2 class="tm-text-uppercase tm-text-gray-light tm-mb">
+                    {{ $t('contact_me') }}
+                </h2>
+            </header>
+            <form @submit.prevent="submitForm" class="tm-contact-form">
+                <div class="tm-form-group">
+                    <input v-model.trim="name" type="text" id="contact_name" name="contact_name" class="form-control"
+                        placeholder="Name">
                     <span v-if="nameError" class="error">{{ nameError }}</span>
-                    </div>
-                    <div class="tm-form-group">
-                    <input v-model.trim="email" type="email" id="contact_email" name="contact_email" class="form-control" placeholder="Email">
+                </div>
+                <div class="tm-form-group">
+                    <input v-model.trim="email" type="email" id="contact_email" name="contact_email" class="form-control"
+                        placeholder="Email">
                     <span v-if="emailError" class="error">{{ emailError }}</span>
-                    </div>
-                    <div class="tm-form-group">
-                    <textarea v-model.trim="message" rows="5" id="contact_message" name="contact_message" class="form-control" placeholder="Message"></textarea>
+                </div>
+                <div class="tm-form-group">
+                    <textarea v-model.trim="message" rows="5" id="contact_message" name="contact_message"
+                        class="form-control" placeholder="Message"></textarea>
                     <span v-if="messageError" class="error">{{ messageError }}</span>
                     <br v-if="generalMessage">
                     <span v-if="generalMessage" class="error">{{ generalMessage }}</span>
-                    </div>
-                    <div class="tm-text-right">
-                    <button type="submit" class="tm-btn tm-btn-secondary tm-btn-pad-big">Send</button>
-                    </div>
-                </form>
-            </div>
-
-            <div class="tm-form-section-tag">
-                <div class="tm-bg-secondary tm-text-white tm-block-pad tm-form-section-tag-inner">
-                    <header>
-                        <h2>Información de contacto</h2>
-                    </header>
-                    <ul>
-                        <li>
-                            <a href="" target="_blank">Instagram</a>
-                        </li>
-                        <li>
-                            <a href="" target="_blank">Facebook</a>
-                        </li>
-                        <li>
-                            <a href="" target="_blank">Whatsapp</a>
-                        </li>
-                    </ul>
-                    <p>Carrer del Deganat 2</p>
-                    <p>Carrer d'Eivissa 3 A</p>
-                    <p>Calle Bonaire, 16 &ndash; 1&ordm;</p>
-                    <p><strong>
-                        Posibilidad de tratamientos a domicilio.
-                    </strong></p>
-                    <a href="tel:+34666795883">{{ $t('call_me') }}</a>
                 </div>
+                <div class="tm-text-right">
+                    <button type="submit" class="tm-btn tm-btn-secondary tm-btn-pad-big">Send</button>
+                </div>
+            </form>
+        </div>
+
+        <div class="tm-form-section-tag">
+            <div class="tm-bg-secondary tm-text-white tm-block-pad tm-form-section-tag-inner">
+                <header>
+                    <h2>{{ $t('contact.title') }}</h2>
+                </header>
+                <ul>
+                    <!-- li> <a href="" target="_blank">Instagram</a> </li -->
+                    <li> <a href="https://www.facebook.com/annachiara.osteospot" target="_blank">Facebook</a> </li>
+                    <li> <a :href="getLinkWhastapp('+34666795883', $t('contact.message_whatsapp'))" target="_blank">Whatsapp</a> </li>
+                </ul>
+                <p>Carrer del Deganat 2</p>
+                <p>Carrer d'Eivissa 3 A</p>
+                <p>Calle Bonaire, 16 &ndash; 1&ordm;</p>
+                <p><strong> {{ $t('contact.treatments_message') }} </strong></p>
+                <a href="tel:+34666795883">{{ $t('call_me') }}</a>
             </div>
         </div>
+    </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      name: "",
-      email: "",
-      message: "",
-      nameError: "",
-      emailError: "",
-      messageError: "",
-      generalMessage: ""
-    };
-  },
-  computed: {
-    formInvalid() {
-      return this.nameError || this.emailError || this.messageError || this.generalMessage;
+    data() {
+        return {
+            name: "",
+            email: "",
+            message: "",
+            nameError: "",
+            emailError: "",
+            messageError: "",
+            generalMessage: "",
+            waiting: false,
+            success: false,
+            errors: false
+        };
     },
-  },
-  methods: {
-    submitForm() {
-        console.log('entrando');
-      // handle form submission
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!this.name.trim() || !emailRegex.test(this.email) || !this.message.trim()) {
-        this.generalMessage = "Please fill all fields";
-        return
-      }
-      this.generalMessage = "";
-      console.log('send mail here!');
+    computed: {
+        formInvalid() {
+            return this.nameError || this.emailError || this.messageError || this.generalMessage;
+        },
     },
-    validateName() {
-      if (!this.name.trim()) {
-        this.nameError = "Name is required";
-      } else {
-        this.nameError = "";
-      }
+    methods: {
+        async submitForm() {
+            // handle form submission
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!this.name.trim() || !emailRegex.test(this.email) || !this.message.trim()) {
+                this.generalMessage = "Please fill all fields";
+                return
+            }
+            this.generalMessage = "";
+            console.log('send mail here!');
+            // Send email using nuxt-mail
+            this.waiting = true;
+            await $fetch('http://localhost:3000/api/contact', {
+                method: 'POST',
+                body: {
+                    name: this.name,
+                    email: this.email,
+                    subject: this.$t('contact.mail_subject'),
+                    message: this.message,
+                },
+            }).then(() => {
+                this.errors = false;
+                this.success = true;
+                this.waiting = false;
+                this.name = "",
+                this.email = "",
+                this.message = ""
+            });
+        },
+        validateName() {
+            if (!this.name.trim()) {
+                this.nameError = "Name is required";
+            } else {
+                this.nameError = "";
+            }
+        },
+        validateEmail() {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(this.email)) {
+                this.emailError = "Please enter a valid email address";
+            } else {
+                this.emailError = "";
+            }
+        },
+        validateMessage() {
+            if (!this.message.trim()) {
+                this.messageError = "Message is required";
+            } else {
+                this.messageError = "";
+            }
+        },
+        getLinkWhastapp(number, message) {
+            var url = 'https://api.whatsapp.com/send?phone=' 
+                + number 
+                + '&text=' 
+                + encodeURIComponent(message)
+
+            return url
+            }
     },
-    validateEmail() {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!emailRegex.test(this.email)) {
-        this.emailError = "Please enter a valid email address";
-      } else {
-        this.emailError = "";
-      }
+    watch: {
+        name() {
+            this.validateName();
+        },
+        email() {
+            this.validateEmail();
+        },
+        message() {
+            this.validateMessage();
+        },
     },
-    validateMessage() {
-      if (!this.message.trim()) {
-        this.messageError = "Message is required";
-      } else {
-        this.messageError = "";
-      }
-    },
-  },
-  watch: {
-    name() {
-      this.validateName();
-    },
-    email() {
-      this.validateEmail();
-    },
-    message() {
-      this.validateMessage();
-    },
-  },
 };
 </script>
 
-<style scoped lang="css">
-.error {
-  color: red;
-  font-size: 14px;
-}
-</style>
+<style scoped lang="css">.error {
+    color: red;
+    font-size: 14px;
+}</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,47 +1,54 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 import { resolve, dirname } from 'node:path'
 import { fileURLToPath } from 'url'
 import VueI18nVitePlugin from '@intlify/unplugin-vue-i18n/vite'
+import { NuxtConfig } from '@nuxt/types'
 
-export default defineNuxtConfig({
-    vite: {
-      plugins: [
-        VueI18nVitePlugin({
-          include: [
-            resolve(dirname(fileURLToPath(import.meta.url)), './locales/*.json')
-          ]
-        })
+const config: NuxtConfig = {
+  vite: {
+    plugins: [
+      VueI18nVitePlugin({
+        include: [
+          resolve(dirname(fileURLToPath(import.meta.url)), './locales/*.json')
+        ]
+      })
+    ]
+  },
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+      titleTemplate: '%s -OsteoSpot by Annachiara',
+      meta: [
+        { name: 'description', content: 'OsteoSpot by Annachiara' }
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'css/all.min.css' },
+        { rel: 'stylesheet', href: 'css/tooplate-style.css' },
+        { rel: 'stylesheet', href: 'css/annachiara-custom.css' },
       ]
+    }
+  },
+  runtimeConfig: {
+    MAILHOST: process.env.MAILHOST,
+    MAILPORT: process.env.MAILPORT,
+    MAILUSER: process.env.MAILUSER,
+    MAILPASSWORD: process.env.MAILPASSWORD,
+    CONTACTMAIL: process.env.CONTACTMAIL
+  }
+  /* mail: {
+    smtp: {
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure: process.env.MAIL_SECURE === 'true',
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+      }
     },
-    app: {
-        head: {
-            charset: 'utf-16',
-            viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
-            titleTemplate: '%s -OsteoSpot by Annachiara',
-            meta: [
-              // <meta name="description" content="My amazing site">
-              { name: 'description', content: 'OsteoSpot by Annachiara' }
-            ],
-            link: [
-                { rel: 'stylesheet', href: 'css/all.min.css' },
-                { rel: 'stylesheet', href: 'css/tooplate-style.css' },
-                { rel: 'stylesheet', href: 'css/annachiara-custom.css' },
-            ]
-        }
-      },
-      /*modules: [
-        ['nuxt-mail', {
-          message: {
-            to: 'asnete@gmail.com',
-          },
-          smtp: {
-            host: 'smtp.dreamhost.com',
-            port: 465,
-            auth: {
-              user: 'info@osteorevolucion.com',
-              pass: '4nn4ch14r4_'
-            },
-          },
-        }],
-      ]*/
-})
+    from: process.env.MAIL_FROM,
+  } */
+}
+
+export default config
